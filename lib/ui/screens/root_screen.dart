807197abae85c147b4f2f8749ac7f8ce20befc5f
Shengfeng Li <cs.shengfeng.li@gmail.com>
2019-08-29 19:44:22 -0500

    Do not show sign in screen if already logged in

--- lib/ui/screens/root_screen.dart
+++ lib/ui/screens/root_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lunarcalendar/ui/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RootScreen extends StatefulWidget {
   @override
@@ -9,6 +10,26 @@ class RootScreen extends StatefulWidget {
 class _RootScreenState extends State<RootScreen> {
   @override
   Widget build(BuildContext context) {
-    return SignInScreen();
+    return new StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return new Container(
+            color: Colors.white,
+          );
+        } else {
+          if (snapshot.hasData) {
+//            return new MainScreen(
+//              firebaseUser: snapshot.data,
+//            );
+            return Container(
+              child: Text("You've already signed in."),
+            );
+          } else {
+            return SignInScreen();
+          }
+        }
+      },
+    );
   }
 }
