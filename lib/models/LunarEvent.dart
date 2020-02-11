47fb102d7e0c635757a1ad0a8c9c13b2ee5be66d
Shengfeng Li <cs.shengfeng.li@gmail.com>
2020-02-10 22:53:43 -0600

    Add LunarEvent dataclass

---
+++ lib/models/LunarEvent.dart
@@ -0,0 +1,43 @@
+import 'package:dataclass/dataclass.dart';
+import 'package:uuid/uuid.dart';
+import 'dart:collection';
+
+class Reminder {
+  String method = "popup";
+  var count = 1;
+  String type = "days";
+  String time = "09:00";
+}
+
+enum RepeatType { NO_REPEAT, MONTHLY, ANNUALLY }
+
+@dataClass
+class LunarEvent {
+  final Uuid id = Uuid();
+  String summary = "";
+  String start = "01-01";
+  String end = "01-01";
+  String location;
+  int repeat = 0;
+  RepeatType repeatType = RepeatType.ANNUALLY;
+  final SplayTreeMap<int, Reminder> reminderMap = new SplayTreeMap();
+
+  LunarEvent({this.summary,
+    this.start,
+    this.end,
+    this.location,
+    this.repeat,
+    this.repeatType});
+
+  @override
+  bool operator ==(other) {
+    if (identical(this, other)) return true;
+    if (other is! LunarEvent) return false;
+
+    return true &&
+        this.id == other.id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
+}
