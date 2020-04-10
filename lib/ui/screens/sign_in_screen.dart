d9c7b864859d3a0aa3285a8c16749e05b8a5dd5b
レスタ <525170139fsl@gmail.com>
2020-04-09 20:11:21 -0500

    Smaller the google image size

--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -115,7 +115,7 @@ class _SignInPageState extends State<SignInScreen> {
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Image(image: AssetImage("assets/google_logo.png"), height: 25.0),
             Padding(
               padding: const EdgeInsets.only(left: 10),
               child: Text(
